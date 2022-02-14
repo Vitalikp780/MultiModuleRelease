3f28ef4b7d43f38de2087e53b529ead81844827c
Vitaliy Sakaly <Vitaliy.Sakaly@vanongo.com>
2022-02-14 17:49:20 +0200

    first change 0.7
    first change 0.8

--- child/src/main/java/Main.java
+++ child/src/main/java/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("hello git rebase i control");
     }
 }
